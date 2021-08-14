bece6ebb1f2d64ace38afd6cc602c5dfac785d28
Fuad Akash <arif.fuad.cse@ulab.edu.bd>
2021-08-14 14:03:58 +0600

    make it better

--- resources/views/Pages/indes.blade.php
+++ resources/views/Pages/indes.blade.php
@@ -13,7 +13,7 @@
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{asset('css/styles.css')}}" rel="stylesheet" />
+        <link href="{{secure_asset('css/styles.css')}}" rel="stylesheet" />
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -167,6 +167,6 @@
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="{{asset('js/scripts.js')}}"></script>
+        <script src="{{secure_asset('js/scripts.js')}}"></script>
     </body>
 </html>
