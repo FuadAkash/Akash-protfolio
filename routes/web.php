2e9b29416cd12354141d313490b7997ed8c9fdd7
Fuad Akash <arif.fuad.cse@ulab.edu.bd>
2021-08-20 12:00:49 +0600

    bug fixing a

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-Route::get('/', [App\Http\Controllers\PagesController::class,'index'])->name('home');
+Route::get('/', [App\Http\Controllers\PagesController::class,'index'])->name('');
 
 Route::prefix('admin')->group(function(){
     
